1f0b98d5de857fadd6d142bc48be84200e5e219d
sonnn1812 <black.spider.1995@gmail.com>
2020-05-19 23:57:48 +0700

    [Init] Complete project

---
+++ src/main/java/com/soa/rest/CalculatorController.java
@@ -0,0 +1,19 @@
+package com.soa.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    @PostMapping("/sum")
+    public int sum(@RequestBody SumRequestDTO request) {
+        return request.getFirstNumber() + request.getSecondNumber();
+    }
+
+    @GetMapping("/sqr/{number}")
+    public double sqr(@PathVariable("number") Integer number) {
+        return Math.pow(number, 2);
+    }
+}
+
---
+++ src/main/java/com/soa/rest/SumRequestDTO.java
@@ -0,0 +1,24 @@
+package com.soa.rest;
+
+
+public class SumRequestDTO {
+
+    public int firstNumber;
+    public int secondNumber;
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+}
